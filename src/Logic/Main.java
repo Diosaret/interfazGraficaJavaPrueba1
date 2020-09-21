7d7eb524d12446dc4e1490aea6f4f63f2f87bf59
Marycielo <marycielooberto236@gmail.com>
2020-09-21 18:11:48 -0300

    Práctica de Interfaz Grafica en java

---
+++ src/Logic/Main.java
@@ -0,0 +1,19 @@
+
+package Logic;
+
+import Gui.Principal;
+
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+       
+        Principal ventana = new Principal();
+        
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        
+    }
+    
+}
---
+++ src/Logic/Operacion.java
@@ -0,0 +1,42 @@
+
+package Logic;
+
+public class Operacion {
+    
+    
+    public double sumar (double num1, double num2){
+        
+        double resultado = num1 + num2;
+        
+        return (resultado);
+        
+        
+    }
+    
+    
+    public double restar (double num1 , double num2){
+        
+        double resultado = num1 - num2;
+        
+        return (resultado);
+        
+    }
+    
+    
+    public double multiplicacion (double num1, double num2){
+        
+        double resultado = num1 * num2;
+        return (resultado);
+        
+    } 
+    
+    
+    public double division (double num1, double num2){
+        
+        double resultado = num1 / num2;
+        return (resultado);
+    }
+    
+    
+    
+}
